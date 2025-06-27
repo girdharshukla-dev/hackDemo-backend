4afdec8047df9b1e3cce9709315ac346a662f838
Girdhar Shukla <girdharshukla26@gmail.com>
2025-06-28 03:59:37 +0530

    cors issue 1 resolve 5

--- controllers/userLogin.controller.js
+++ controllers/userLogin.controller.js
@@ -27,8 +27,8 @@ async function userLogin(req, resp) {
         const jwtToken = setUserToken({id: user.id , email: user.email});
         resp.cookie("jwtToken", jwtToken, {
             httpOnly: true,
-            sameSite: "Strict",
-            secure: process.env.NODE_ENV==="production"
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+            secure: process.env.NODE_ENV === "production"
         });
         return resp.status(200).json({ message: "Login success" , user : {id:user.id , email: user.email}, token:jwtToken });
     } catch (err) {
